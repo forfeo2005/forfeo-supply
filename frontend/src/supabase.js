6a9fb6c75a7db33257b9a5344192874b72ebfe6d
forfeo2005 <forfeo2005@gmail.com>
2026-02-01 18:35:36 -0500

    Update supabase.js

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Configuration de la connexion à ta base de données
+// URL de ton projet Supabase
 const supabaseUrl = 'https://pvpztslxriczpooaicvc.supabase.co';
-const supabaseKey = 'sb_publishable_EN5HvTEviUmhSca3nNcYXg_N9hgj...'; // Ta clé publique (raccourcie pour l'exemple, assure-toi d'avoir la complète si possible, sinon reprends celle de ta capture)
 
+// Ta Clé Publique (Safe pour le frontend)
+const supabaseKey = 'sb_publishable_EN5HvTEviUmhSea3nNcYXg_N9hgjFim';
+
+// Création du client pour se connecter à la base de données
 export const supabase = createClient(supabaseUrl, supabaseKey);
