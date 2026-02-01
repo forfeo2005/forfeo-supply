4e9632d03d5ac7508498d4586a8f2946a2dd69a1
forfeo2005 <forfeo2005@gmail.com>
2026-02-01 18:30:13 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // C'est cette ligne qui charge le design Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
