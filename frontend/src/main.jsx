ecd7ec1d211eb540b5ea88d935e4dba4ea0e0e4b
forfeo2005 <forfeo2005@gmail.com>
2026-02-01 20:03:52 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // C'est cette ligne qui charge le design Tailwind
+import { CartProvider } from './context/CartContext'; // <--- IMPORT DU PANIER
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* On enveloppe l'App pour que le panier soit accessible partout */}
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
 )
